19f1c349803f25cb67bc2625ee86267358bbd743
chaincpp31 <chaincpp31@gmail.com>
2020-10-07 23:26:12 +0700

    update index.js in Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,16 @@
 import React from "react";
-import "main.css";
+import "./main.css";
 
 function Navbar(){
     return(
-        <div className="navbar-bg"></div>
+        <div className="nav-container">
+            <div className="logo"></div>
+            <span className="nav-button-container">
+            <span className="nav-button">About</span>
+            <span className="nav-button">Match</span>
+            <span className="nav-button">Login</span>
+            </span>
+        </div>
     )
 }
 
