b797fef81a7155ae5c8c5ef59bde386e54368bbd
chaincpp31 <chaincpp31@gmail.com>
2020-10-13 14:17:51 +0700

    add routers

---
+++ src/config/routes.js
@@ -0,0 +1,19 @@
+import { lazy } from "react";
+
+export default {
+  Home: {
+    path: "/",
+    exact: true,
+    component: lazy(() => import("../pages/home")),
+  },
+  Game: {
+    path: "/game",
+    exact: true,
+    component: lazy(() => import("../pages/Game")),
+  },
+  About: {
+    path: "/about",
+    exact: true,
+    component: lazy(() => import("../pages/About")),
+  },
+};
