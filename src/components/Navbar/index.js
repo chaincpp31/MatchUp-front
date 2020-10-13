300bf493cda72fedb868d52fdd80f6b210028a09
chaincpp31 <chaincpp31@gmail.com>
2020-10-13 14:17:14 +0700

    edit navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,16 @@
 import React from "react";
-import "./main.css";
+import { Link } from "react-router-dom";
+
+// import "./main.css";
 
 function Navbar(){
     return(
         <div className="nav-container">
             <div className="logo"></div>
             <span className="nav-button-container">
+            <span className="nav-button"><Link to="/">Home</Link></span>
+            <span className="nav-button">Game</span>
             <span className="nav-button">About</span>
-            <span className="nav-button">Match</span>
             <span className="nav-button">Login</span>
             </span>
         </div>
