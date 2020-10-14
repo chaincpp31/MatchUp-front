5faabe0b5c7c161d5ebeacae280dc2813693a01e
jeffyso <jeffyzaja@gmail.com>
2020-10-14 20:36:31 +0700

    update-App.js-GlobalStyle-import

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Register from "./pages/Register";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import routes from "./config/routes";
 import withHelmet from "./utils/with-Helmet"
+import GlobalStyle from './components/GlobalStyle'
 
 function App() {
   return (
     <>
+    <GlobalStyle/>
     <Router>
         <Navbar />
         <Suspense fallback="...loading">
