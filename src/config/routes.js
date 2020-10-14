5258a84d288238350c73117a3e4c47961f7886ff
chaincpp31 <chaincpp31@gmail.com>
2020-10-14 19:16:45 +0700

    comment all rout

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,19 +1,24 @@
-import { lazy } from "react";
+// import { lazy } from "react";
 
-export default {
-  Home: {
-    path: "/",
-    exact: true,
-    component: lazy(() => import("../pages/home")),
-  },
-  Game: {
-    path: "/game",
-    exact: true,
-    component: lazy(() => import("../pages/Game")),
-  },
-  About: {
-    path: "/about",
-    exact: true,
-    component: lazy(() => import("../pages/About")),
-  },
-};
+// export default {
+//   Home: {
+//     path: "/",
+//     exact: true,
+//     component: lazy(() => import("../pages/home")),
+//   },
+//   Game: {
+//     path: "/game",
+//     exact: true,
+//     component: lazy(() => import("../pages/Game")),
+//   },
+//   About: {
+//     path: "/about",
+//     exact: true,
+//     component: lazy(() => import("../pages/About")),
+//   },
+//   Register: {
+//       path: "/register",
+//       exact: true,
+//       component: lazy(() => import("../pages/Register"))
+//   }
+// };
