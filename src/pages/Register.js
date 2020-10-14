9c8e170265868fe1e596b484a887014e690961fd
chaincpp31 <chaincpp31@gmail.com>
2020-10-14 14:23:35 +0700

    add register file

---
+++ src/pages/Register.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0;
+  display: flex;
+  margin: 2rem;
+`;
+const InputForm = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: antiquewhite;
+  border-radius: 5%;
+  padding-right: 30vw;
+  white-space: normal;
+`;
+
+function Register() {
+    return(
+        <>
+        <Container>
+        <InputForm/>
+        tffffffffffffffffffffffffffffy
+        ftydtyftydrft
+        </Container>
+        </>
+    )
+};
+
+export default Register;
