e64a8d0f0147358cecf860d78534147fcd289788
jeffyso <jeffyzaja@gmail.com>
2020-10-17 15:40:53 +0700

    Add-Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import styled from 'styled-components'
+import backgroundImage from '../assets/LogoBackground.png'
+import BackgroundBlock from '../components/BackgroundBlock'
+
+function Home () {
+  return (
+    <>
+      <BackgroundBlock src={backgroundImage}>
+      </BackgroundBlock>
+    </>
+  )
+}
+
+export default Home
