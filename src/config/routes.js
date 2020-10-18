af5685d265f9262eb0a022273a6e0d4827b88dba
jeffyso <jeffyzaja@gmail.com>
2020-10-19 02:54:43 +0700

    update-route

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,24 +1,29 @@
-// import { lazy } from "react";
+import { lazy } from "react";
 
-// export default {
-//   Home: {
-//     path: "/",
-//     exact: true,
-//     component: lazy(() => import("../pages/home")),
-//   },
-//   Game: {
-//     path: "/game",
-//     exact: true,
-//     component: lazy(() => import("../pages/Game")),
-//   },
-//   About: {
-//     path: "/about",
-//     exact: true,
-//     component: lazy(() => import("../pages/About")),
-//   },
-//   Register: {
-//       path: "/register",
-//       exact: true,
-//       component: lazy(() => import("../pages/Register"))
-//   }
-// };
+export default {
+  Home: {
+    path: "/",
+    exact: true,
+    component: lazy(() => import("./../pages/Home")),
+  },
+  CreateEvent: {
+    path: "/createEvent",
+    exact: true,
+    component: lazy(() => import("./../pages/CreateEvent")),
+  },
+  // Game: {
+  //   path: "/game",
+  //   exact: true,
+  //   component: lazy(() => import("../pages/Game")),
+  // },
+  About: {
+    path: "/about",
+    exact: true,
+    component: lazy(() => import("../pages/About")),
+  },
+  Register: {
+      path: "/register",
+      exact: true,
+      component: lazy(() => import("../pages/Register"))
+  }
+};
