1d436bfc3ad0a0d9b3a6d4ceed1dfdd81512380b
jeffyso <jeffyzaja@gmail.com>
2020-10-19 02:48:27 +0700

    update-app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Suspense} from 'react';
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/index";
 // import InformationEvent from "./pages/InformationEvent";
-import Register from "./pages/Register";
+// import Register from "./pages/Register";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import routes from "./config/routes";
-import withHelmet from "./utils/with-Helmet"
-import GlobalStyle from './components/GlobalStyle'
+// import routes from "./config/routes";
+import withHelmet from "./utils/with-Helmet";
+import GlobalStyle from './components/GlobalStyle';
+import Footer from './components/Footer';
+import routes from './config/routes'
+
+// import Home from './pages/Home';
+// import Register from './pages/Register'
+// import CreateEvent from './pages/CreateEvent';
 
 function App() {
   return (
@@ -14,14 +20,17 @@ function App() {
     <Router>
         <Navbar />
         <Suspense fallback="...loading">
-          <switch>
+          <Switch>
             {Object.keys(routes).map((routeKey) => (
-              <Route key={routeKey} {...routes[routeKey]} />
-            ))}
-          </switch>
+               <Route key={routeKey} 
+               {...routes[routeKey]} 
+              /> 
+              ))} 
+          </Switch>
         </Suspense>
       </Router>
-      <Register/>
+      {/* <Footer/> */}
+      {/* <Register/> */}
       {/* <InformationEvent /> */}
     </>
   );
