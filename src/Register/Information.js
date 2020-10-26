4c8e606df127a6beaa8dbfe27f96aa67d3901de2
jeffyso <jeffyzaja@gmail.com>
2020-10-27 05:25:12 +0700

    update-information

--- src/Register/Information.js
+++ src/Register/Information.js
@@ -1,8 +1,8 @@
-import React, { Fragment } from 'react'
+import React, { Fragment ,useState,useEffect} from 'react'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
-
+import RegisterAPI from '../services/RegisterApi'
 const useStyles = makeStyles(theme => ({
   root: {
     margin: '-1rem 0 2rem 0',
@@ -28,10 +28,44 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-export default function Information({ formProps: { register, errors }, data }) {
+export default function Information({ formProps: { register, errors }, data,callback }) {
   const classes = useStyles()
   const { firstName, lastName, name } = data[0]
+  const [values, setValues] = useState({
+    name:'',
+    user_name:'',
+    password:'',
+    first_name:'',
+    last_name:'',
+    email:'',
+    phone_number:'',
+    age:'',
+    birth_day:'',
+    status:''
 
+  })
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  
+  const handleChange = e => {
+    const { name, value } = e.target
+    setValues({
+      ...values,
+      [name]: value
+    })
+  }
+  
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(values, '1')
+    RegisterAPI(values)
+    // console.log(values)
+  }
+  
+  useEffect(() => {
+    if (isSubmitting) {
+      callback()
+    }
+  }, [])
   return (
     <Fragment>
       <div className={classes.root}>
@@ -54,7 +88,9 @@ export default function Information({ formProps: { register, errors }, data }) {
                   variant="outlined"
                   inputRef={register}
                   error={!!errors.name}
-                  defaultValue={name}
+                  defaultValue={values.name}
+                  // value={values.name}
+                  onChange={handleChange}
                 />
                 {errors.name && (
                   <p className={classes.errorMessage}>{errors.name.message}</p>
@@ -72,7 +108,9 @@ export default function Information({ formProps: { register, errors }, data }) {
               variant="outlined"
               inputRef={register}
               error={!!errors.firstName}
-              defaultValue={firstName}
+              defaultValue={values.first_name}
+              // value={values.first_name}
+            onChange={handleChange}
             />
             {errors.firstName && (
               <p className={classes.errorMessage}>{errors.firstName.message}</p>
@@ -88,7 +126,9 @@ export default function Information({ formProps: { register, errors }, data }) {
               variant="outlined"
               inputRef={register}
               error={!!errors.lastName}
-              defaultValue={lastName}
+              defaultValue={values.last_name}
+              // value={values.last_name}
+            onChange={handleChange}
             />
             {errors.lastName && (
               <p className={classes.errorMessage}>{errors.lastName.message}</p>
