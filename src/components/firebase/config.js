731eb6e0f7767a5383a3792ac7650316b8b800bc
chaincpp31 <chaincpp31@gmail.com>
2020-10-26 22:51:51 +0700

    create config firebase API

---
+++ src/components/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyADnKq7AutEsVIZEZSWd6Sfxx-RJT5kfz8',
+  authDomain: 'matchup-login-c9110.firebaseapp.com',
+  databaseURL: 'https://matchup-login-c9110.firebaseio.com',
+  projectId: 'matchup-login-c9110',
+  storageBucket: 'matchup-login-c9110.appspot.com',
+  messagingSenderId: '810892166217',
+  appId: '1:810892166217:web:50d569dc84ce84cf08eb98',
+  measurementId: 'G-XBB4RYM7SX'
+}
+export default firebaseConfig
