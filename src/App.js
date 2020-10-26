28e3f8e541a8508a990dbf066b82b09882bebec6
jeffyso <jeffyzaja@gmail.com>
2020-10-27 02:36:00 +0700

    update-app.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           </Switch>
         </Suspense>
       </Router>
-      {/* <Footer/> */}
+      <Footer/>
       {/* <Register/> */}
       {/* <InformationEvent /> */}
     </>
