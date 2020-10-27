20c2e86b999415dda4b02adfd73a5305bef2d254
chaincpp31 <chaincpp31@gmail.com>
2020-10-28 02:32:48 +0700

    update Navbar index

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect,useContext} from 'react'
 import auth from '../firebase'
 import {
   NavButtonContainer,
@@ -19,11 +19,15 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import styled from 'styled-components'
+import Home from '../../pages/Home'
+import { StoreContext } from '../../Context/store'
 
   
 
 
-const Navbar = ({ setSession }) => {
+const Navbar = () => {
+const {session:[session,setSession]} = useContext(StoreContext)
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -43,6 +47,14 @@ const Navbar = ({ setSession }) => {
         errorMessage: error.message
       });
     } setOpen(false)
+  };
+  const handleLogout = () => {
+    auth.signOut().then(response => {
+      setSession({
+        isLoggedIn: false,
+        currentUser: null
+      })
+    })
   };
   const handleRegister = async () => {
     try {
@@ -77,6 +89,52 @@ const Navbar = ({ setSession }) => {
   `
   const [open, setOpen] = React.useState(false)
 
+   const DialogActions = styled.div`
+     display: flex;
+     justify-content: center;
+     padding: 8px 24px;
+   `
+
+   const DialogContentText = styled.div`
+     text-align: center;
+   `
+   const Context = styled.div`
+     display: flex;
+     flex-direction: column;
+     justify-content: space-around;
+     height: 55%;
+   `
+   const BannerWrap = styled.div`
+     display: flex;
+     img {
+       width: 62%;
+     }
+   `
+   const DialogWrapper = styled.div`
+     width: 100%;
+     padding: 4%;
+     background-color: #dfcfaf;
+     color: rgb(28, 29, 34);
+   `
+   const Button = styled.div`
+     /* background-color: rgba(28, 29, 34, 1); */
+     border: 1px solid #1c1d22;
+     border-radius: 7px;
+     padding: 20px 20px;
+     color: #dfcfaf;
+     font-size: 16px;
+     text-align: center;
+     font-weight: 700;
+     width: 10%;
+     margin: 0 2% 0 2%;
+     transition: 0.2s;
+     &:hover {
+       color: #dfcfaf;
+       background-color: rgba(28, 29, 34, 0.9);
+       border: 1px solid #1c1d22;
+     }
+   `
+
   const handleClickOpen = () => {
     setOpen(true)
   }
@@ -86,6 +144,7 @@ const Navbar = ({ setSession }) => {
   }
 
   const isScroll = useScroll(20)
+
 
   return (
     <NavContainer isScroll={isScroll}>
@@ -99,50 +158,60 @@ const Navbar = ({ setSession }) => {
               <Link to="/about">ABOUT</Link>
             </Action>
             <Action>
-              <Link onClick={handleClickOpen}>LOGIN</Link>
-              <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Login</DialogTitle>
-                <DialogContent>
-                  <TextField
-                    autoFocus
-                    margin="dense"
-                    id="username"
-                    label="Username"
-                    type="username"
-                    onChange={handleUsername}
-                    fullWidth
-                  />
-                  <TextField
-                    margin="dense"
-                    id="password"
-                    label="Password"
-                    type="password"
-                    onChange={handlePassword}
-                    fullWidth
-                  />
-                </DialogContent>
-                <DialogActions>
-                  <DialogContentText>
-                    Are you have account?{' '}
-                    <Link to="/register" onClick={handleClose} color="blue">
-                      <BtnRgt>register</BtnRgt>
-                    </Link>
-                  </DialogContentText>
-                  <Button onClick={handleClose} color="primary">
-                    Cancel
-                  </Button>
-                  <Button onClick={handleLogin} color="primary">
-                    Login
-                  </Button>
-                </DialogActions>
-              </Dialog>
+              <Link
+                onClick={session.isLoggedIn ? handleLogout : handleClickOpen}>
+                {session.isLoggedIn ? 'Logout' : 'Login'}
+              </Link>
+              {!session.isLoggedIn ? (
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="form-dialog-title">
+                  {/* <DialogTitle id="form-dialog-title">Login</DialogTitle> */}
+                  <DialogContent>
+                    <TextField
+                      autoFocus
+                      margin="dense"
+                      id="username"
+                      label="Username"
+                      type="username"
+                      onChange={handleUsername}
+                      fullWidth
+                    />
+                    <TextField
+                      margin="dense"
+                      id="password"
+                      label="Password"
+                      type="password"
+                      onChange={handlePassword}
+                      fullWidth
+                    />
+                  </DialogContent>
+                  <DialogActions>
+                    <DialogContentText>
+                      No account?{' '}
+                      <Link
+                        to="/register"
+                        onClick={handleRegister}
+                        color="blue">
+                        <BtnRgt>register</BtnRgt>
+                      </Link>
+                    </DialogContentText>
+                    <Button onClick={handleClose} color="primary">
+                      <Link>Cancel</Link>
+                    </Button>
+                    <Button onClick={handleLogin} color="primary">
+                      Login
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              ) : (
+                ''
+              )}
             </Action>
-              <Action>
-                <Link to="/CreateEvent">Create</Link>
-              </Action>
+            <Action>
+              <Link to="/CreateEvent">Create</Link>
+            </Action>
           </NavButtonContainer>
         </ActionContainer>
       </Wrapper>
