5a06a2aeccbaec6dace5e031d5a2540e297a168d
chaincpp31 <chaincpp31@gmail.com>
2020-10-28 02:35:39 +0700

    update styeles navbar

--- src/components/Navbar/Styles.js
+++ src/components/Navbar/Styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-
 export const NavContainer = styled.div`
   width: 100vw;
   position: fixed;
@@ -8,19 +7,16 @@ export const NavContainer = styled.div`
   color: #fff;
   transition: background-color 0.5s;
 `
-
 export const Wrapper = styled.div`
   display: flex;
   flex: 1;
-  padding: .3rem;
+  padding: 0.3rem;
 `
-
 export const ActionContainer = styled.ul`
   display: flex;
   flex: 1;
   justify-content: flex-end;
 `
-
 export const Action = styled.li`
   padding: 1rem;
   display: flex;
@@ -28,24 +24,19 @@ export const Action = styled.li`
   align-content: center;
   transition: background-color 0.2s;
   border-radius: 10px;
-
   &:hover {
-    padding: 1rem;
-    border-radius: 10px;
+    border-radius: 7px;
     background-color: ${props => (props.isScroll ? '#000' : '#fff')};
-    color:#000;
+    color: #000;
   }
 `
-
 export const NavButtonContainer = styled.div`
   display: flex;
   text-align: center;
   list-style-type: none;
 `
 
-
 export const Logo = styled.div`
   display: flex;
   background-image: url(${props => props.src});
-
 `
